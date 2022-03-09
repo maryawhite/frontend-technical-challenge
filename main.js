67e5aff37a7de000f9e9fcbf6a2840e121d7a795
Mary White <mary.a.white28@gmail.com>
2022-03-08 21:50:57 -0500

    added cards with data

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ console.log("Hello!!!");
 
 $(document).ready(function (){
 
+/** THIS FIRST FETCH GETS THE LIST OF POKEMONS */
+
     fetch("https://pokeapi.co/api/v2/pokemon?limit=100&offset=200", {
         "method": "GET",
         "cache": "no-cache"
@@ -15,11 +17,10 @@ $(document).ready(function (){
 
                     let myPokemons = "";
                     for (let i = 0; i < resultsObj.results.length; i++) {
-                        // myPokemons += `<div><h4>${resultsObj.results[i].name}</h4></div>`
-                        // remember to use ${} around your data
+
                         let url = resultsObj.results[i].url;
 
-                        //do a fetch inside the fetch to get the url for each pokemon
+                        /** THIS SECOND FETCH INSIDE OF THE FOR LOOP GETS THE INFO OUT OF THE URL THAT WAS PROVIDED IN THE FIRST FETCH */
                         fetch(url, {
                             "method": "GET",
                             "cache": "no-cache"
@@ -29,20 +30,51 @@ $(document).ready(function (){
                                     .then((resultsTwo) => {
                                         console.log(resultsTwo);
 
-                                        // let myPokemons = "";
-                                            myPokemons += `<div class="card-deck"><div class="card"><img class="card-img-top" src=${resultsTwo.sprites.front_default}><div class="card-body"><div><h4 class="card-title">${resultsTwo.name}</h4><p class="card-text">Weight: ${resultsTwo.weight}</p></div></div></div></div>`
+                                        /** FOREACH LOOP TO GET THE POKEMON'S TYPE */
+                                        let types = resultsTwo.types;
+                                        let itsType = "";
+                                        types.forEach(function(item, index, arr){
+                                            /** IF BLOCK TO GET RID OF THE TRAILING COMMA WHEN THERE'S ONLY ONE ITEM */
+                                            if(index === types.length -1 ){
+                                                itsType += item.type.name;
+                                            } else {
+                                                itsType += item.type.name + ", ";
+                                            }
+
+                                        })
+
+                                        /** FOREACH LOOP TO GET THE POKEMON'S ABILITIES */
+                                        let abilities = resultsTwo.abilities;
+                                        let itsAbilities = "";
+                                        abilities.forEach(function(item, index){
+                                            if(index === abilities.length - 1) {
+                                                itsAbilities += item.ability.name;
+                                            } else {
+                                                itsAbilities += item.ability.name + ", ";
+                                            }
+                                        })
+
+                                        /** FOREACH LOOP TO GET THE POKEMON'S STATS */
+                                        let stats = resultsTwo.stats;
+                                        let itsStats = "";
+                                        stats.forEach(function(item, index){
+                                            if(index === stats.length - 1) {
+                                                itsStats += item.stat.name + " " + item.base_stat;
+                                            } else {
+                                                itsStats += item.stat.name + " " + item.base_stat + ", ";
+                                            }
+                                        })
+
+                                        /** PUTTING INFORMATION INTO THE CARD VIA THE DIV IN THE INDEX.HTML */
+                                        myPokemons += `<div class="card-deck col-sm-6 col-md-4 col-lg-4"><div class="card align-items-center mb-4"><img class="card-img-top w-50" src=${resultsTwo.sprites.front_default}><div class="card-body text-center"><div><h1 class="card-title">${resultsTwo.name}</h1><p>Base Experience ${resultsTwo.base_experience}</p><p class="card-text">Weight: ${resultsTwo.weight}</p><p>Height: ${resultsTwo.height}</p><p id="types">Type(s): ${itsType}</p><p>Abilities: ${itsAbilities}</p><p>Stats: ${itsStats}</p></div></div></div></div>`
                                         $("#pokemons").html(myPokemons);
+
                                     })
                             })
                     }
                 })
                 .catch(err => console.error(err))
 
-
-
-
-
         });
-
 
 })
